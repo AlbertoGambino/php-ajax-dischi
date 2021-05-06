1078b4ec882f32e54c7da325bd0505d8d0bd1fd2
Alberto Gambino <gambafb@libero.it>
2021-05-06 16:50:05 +0100

    fixing

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
      <!-- vue 2 -->
-     <script src="https://cdn.jsdelivr.net/npm/vue@2.x"></script> -->
+     <script src="https://cdn.jsdelivr.net/npm/vue@2.x"></script>
      <!-- axios -->
      <script src="https://cdn.jsdelivr.net/npm/axios@0.21.1/dist/axios.min.js"></script>
 
@@ -38,5 +38,5 @@
 
         </div>
     </div>
-  </body>  
+  </body>
 </html>
